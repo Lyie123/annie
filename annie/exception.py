eefc5c559a0052c563abfb0355a4b127be2d24ac
Lyie123 <moch.rene@googlemail.com>
2021-10-11 20:12:39 +0200

    Update exception.py

--- annie/exception.py
+++ annie/exception.py
@@ -1,4 +1,4 @@
 class ApiException(Exception):
     def __init__(self, message: str, status_code: int):
         self.status_code = status_code
-        super().__init__(message)
+        self.message = message
