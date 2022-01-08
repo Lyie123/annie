38d8d51efb39d311919afa97710330d3932f8aee
lyie <moch.rene@googlemail.com>
2022-01-08 16:19:56 +0100

    add super support apiexception

--- annie/exception.py
+++ annie/exception.py
@@ -1,4 +1,5 @@
 class ApiException(Exception):
     def __init__(self, message: str, status_code: int):
+        super().__init__(message)
         self.status_code = status_code
         self.message = message
